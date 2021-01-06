467a5136f47087e541b6fb902475fb6161003ee2
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 22:20:21 +0000

    Update Solution.java

--- Day 1 Data Types/src/Solution.java
+++ Day 1 Data Types/src/Solution.java
@@ -1,8 +1,4 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
@@ -18,15 +14,28 @@ public class Solution {
         /* Read and save an integer, double, and String to your variables. */
         // Note: If you have trouble reading the entire String, please go back and
         // review the Tutorial closely.
+        int input_int;
+        input_int = scan.nextInt();
+
+        double input_double;
+        input_double = scan.nextDouble();
+
+        scan.nextLine();
+        String input_string;
+        input_string = scan.nextLine();
 
         /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + input_int);
 
         /* Print the sum of the double variables on a new line. */
+        System.out.println(d + input_double);
 
         /*
          * Concatenate and print the String variables on a new line; the 's' variable
          * above should be printed first.
          */
+
+        System.out.println(s + input_string);
 
         scan.close();
     }
